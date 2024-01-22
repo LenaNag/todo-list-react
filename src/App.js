57376c912080130c718ddc20aceab6bdd69865aa
Alena Nahornaya <nahornaya.alena@gmail.com>
2024-01-22 20:36:55 +0100

    toogle task done interaction

--- src/App.js
+++ src/App.js
@@ -21,6 +21,16 @@ function App() {
     setTasks(tasks => tasks.filter(task => task.id !== id));
   };
 
+  const toogleTaskDone = (id) => {
+    setTasks(tasks => tasks.map(task => {
+      if (task.id === id) {
+        return { ...task, done: !task.done };
+      }
+
+      return task;
+    }));
+  };
+
   return (
     <Container>
       <Header title="Lista zadań" />
@@ -30,7 +40,13 @@ function App() {
       />
       <Section
         title="Lista zadań"
-        body={<Tasks tasks={tasks} hideDone={hideDone} removeTask={removeTask} />}
+        body={
+          <Tasks
+            tasks={tasks}
+            hideDone={hideDone}
+            removeTask={removeTask}
+            toogleTaskDone={toogleTaskDone}
+          />}
         extraHeaderContent={<Buttons tasks={tasks} hideDone={hideDone} toogleHideDone={toogleHideDone} />}
       />
     </Container>
--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -1,12 +1,17 @@
 import "./style.css";
 
-const Tasks = ({ tasks, hideDone, removeTask}) => (
+const Tasks = ({ tasks, hideDone, removeTask, toogleTaskDone }) => (
     <ul className="tasks">
         {tasks.map(task => (
             <li key={task.id}
-                className={`${(task.done && hideDone) ? "tasks__item--hidden" : "tasks__item"}`}
+                className={`tasks__item${(task.done && hideDone) ? " tasks__item--hidden" : ""}`}
             >
-                <button className="tasks__button">{task.done ? "✓" : ""}</button>
+                <button
+                    className="tasks__button"
+                    onClick={() => toogleTaskDone(task.id)}
+                >
+                    {task.done ? "✓" : ""}
+                </button>
                 <span className={`${task.done ? "tasks__item--done" : ""}`}>{task.content}</span>
                 <button className="tasks__button tasks__button--delete" onClick={() => removeTask(task.id)}>×</button>
             </li>
